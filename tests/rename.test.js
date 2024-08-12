7f16c72665432b541dc007455e428ad06fb22e2f
coder_guan@163.com <coder_guan@163.com>
2024-08-12 21:02:31 +0800

    chore(format): prettier style

--- tests/rename.test.js
+++ tests/rename.test.js
@@ -1,46 +1,45 @@
-const path = require("node:path");
-const fs = require("node:fs");
-const { describe, it, expect } = require("@jest/globals")
-const {rimrafSync} = require("rimraf");
-const cpdir = require("../dist").default
+const path = require('node:path');
+const fs = require('node:fs');
+const { describe, it, expect } = require('@jest/globals');
+const { rimrafSync } = require('rimraf');
+const cpdir = require('../dist').default;
 
-describe("cpdir rename", () => {
+describe('cpdir rename', () => {
+  afterEach(() => {
+    const targetPath = path.join(__dirname, '../build');
+    if (fs.existsSync(targetPath)) {
+      rimrafSync(targetPath);
+    }
+  });
 
-    afterEach(() => {
-        const targetPath = path.join(__dirname, "../build")
-        if (fs.existsSync(targetPath)) {
-            rimrafSync(targetPath)            
-        }
-    })
+  it('should rename files', async () => {
+    const sourcePath = path.resolve(__dirname, '../dist');
+    const targetPath = path.resolve(__dirname, '../build');
 
-    it("should rename files", async () => {
-        const sourcePath = path.resolve(__dirname, "../dist")
-        const targetPath = path.resolve(__dirname, "../build")
+    cpdir({
+      from: sourcePath,
+      to: targetPath,
+      renameFiles: { 'index.js.map': '_index.js.map' },
+    }).then(() => {
+      const result = fs.readdirSync(targetPath);
 
-        cpdir({
-            from: sourcePath,
-            to: targetPath,
-            renameFiles: {"index.js.map": "_index.js.map"}
-        }).then(() => {
-            const result = fs.readdirSync(targetPath);
+      expect(result).toContain('_index.js.map');
+    });
+  });
 
-            expect(result).toContain('_index.js.map');
-        })
-    })
+  it('should rename no exist file', async () => {
+    const sourcePath = path.resolve(__dirname, '../dist');
+    const targetPath = path.resolve(__dirname, '../build');
 
-    it("should rename no exist file", async () => {
-        const sourcePath = path.resolve(__dirname, "../dist")
-        const targetPath = path.resolve(__dirname, "../build")
+    cpdir({
+      from: sourcePath,
+      to: targetPath,
+      renameFiles: { '_index.js.map': 'index.js.map' },
+    }).then(() => {
+      const sourceFiles = fs.readdirSync(targetPath);
+      const targetFiles = fs.readdirSync(targetPath);
 
-        cpdir({
-            from: sourcePath,
-            to: targetPath,
-            renameFiles: {"_index.js.map": "index.js.map"}
-        }).then(() => {
-            const sourceFiles = fs.readdirSync(targetPath);
-            const targetFiles = fs.readdirSync(targetPath);
-
-            expect(sourceFiles).toMatchObject(targetFiles);
-        })
-    })
-})
+      expect(sourceFiles).toMatchObject(targetFiles);
+    });
+  });
+});
--- tests/replacements.test.js
+++ tests/replacements.test.js
@@ -1,38 +1,37 @@
-const path = require("node:path");
-const fs = require("node:fs");
-const { describe, it, expect } = require("@jest/globals")
-const {rimrafSync} = require("rimraf");
-const cpdir = require("../dist").default
+const path = require('node:path');
+const fs = require('node:fs');
+const { describe, it, expect } = require('@jest/globals');
+const { rimrafSync } = require('rimraf');
+const cpdir = require('../dist').default;
 
-describe("cpdir replacements", () => {
+describe('cpdir replacements', () => {
+  afterEach(() => {
+    const targetPath = path.join(__dirname, '../build');
+    if (fs.existsSync(targetPath)) {
+      rimrafSync(targetPath);
+    }
+  });
 
-    afterEach(() => {
-        const targetPath = path.join(__dirname, "../build")
-        if (fs.existsSync(targetPath)) {
-            rimrafSync(targetPath)            
-        }
-    })
+  it('should replacements %PORJECTNAME% replace __project_name__', async () => {
+    const sourcePath = path.resolve(__dirname, '../dist');
+    const targetPath = path.resolve(__dirname, '../build');
 
-    it("should replacements %PORJECTNAME% replace __project_name__", async () => {
-        const sourcePath = path.resolve(__dirname, "../dist")
-        const targetPath = path.resolve(__dirname, "../build")
+    const readmePath = path.join(sourcePath, 'README.md');
+    const content = '%PORJECTNAME%';
+    fs.writeFileSync(readmePath, content, { encoding: 'utf8' });
 
-        const readmePath = path.join(sourcePath, "README.md");
-        const content = "%PORJECTNAME%"
-        fs.writeFileSync(readmePath, content, { encoding: "utf8" });
+    await cpdir({
+      from: sourcePath,
+      to: targetPath,
+      replacements: [{ filename: ['**.md'], PORJECTNAME: '__project_name__' }],
+    }).then(() => {
+      const readmeTargetPath = path.join(targetPath, 'README.md');
 
-        await cpdir({
-            from: sourcePath,
-            to: targetPath,
-            replacements: [{filename: ["**.md"], PORJECTNAME: "__project_name__"}]
-        }).then(() => {
-            const readmeTargetPath = path.join(targetPath, "README.md");
+      const result = fs.readFileSync(readmeTargetPath, { encoding: 'utf8' });
 
-            const result = fs.readFileSync(readmeTargetPath, {encoding: "utf8"});
+      expect(result).toContain('__project_name__');
 
-            expect(result).toContain('__project_name__');
-
-            rimrafSync(readmeTargetPath);
-        })
-    })
-})
+      rimrafSync(readmePath);
+    });
+  });
+});
